e2083892be1ac2d7f30377b2a811157c68b34869
Alexis <alexis@cvofhope.com>
2019-08-17 09:07:36 +1000

    calc median for an odd set

--- stats.nim
+++ stats.nim
@@ -7,5 +7,13 @@ proc average(data: seq[int]): int =
 
 const mean = average
 
+proc medianOdd(data: seq[int]): int =
+  let nth = (data.len + 1) div 2
+  return data[nth]
+
+proc median(data: seq[int]): int =
+    return medianOdd(data)
+
 echo "Average: ", average(data)
 echo "Mean: ", mean(data)
+echo "Median: ", median(data)
