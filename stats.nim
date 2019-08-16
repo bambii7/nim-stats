0fc50c7c1ca6d5fbab6c928f3b1a983a8e92612a
Alexis <alexis@cvofhope.com>
2019-08-17 09:46:02 +1000

    proxy median to even or odd procs

--- stats.nim
+++ stats.nim
@@ -7,9 +7,9 @@ proc average(data: seq[int]): int =
 
 const mean = average
 
-proc medianOdd(data: seq[int]): int =
-  let nth = (data.len + 1) div 2
-  return data[nth-1]
+proc medianOdd(data: seq[float|int]): float =
+  let nth:int = (data.len + 1) div 2
+  return float(data[nth-1])
 
 assert medianOdd(@[0, 1, 2, 3, 4, 5, 6]) == 3
 assert medianOdd(@[10, 34, 56, 89, 99, 101, 102]) == 89
@@ -23,8 +23,14 @@ proc medianEven(data: seq[float|int]): float =
 assert medianEven(@[0.0,1.0,2.0,3.0,4.0,5.0,6.0,7.0]) == 3.5
 assert medianEven(@[10, 34, 54, 56, 89, 99, 101, 102]) == 72.5
 
-proc median(data: seq[int]): int =
+proc median(data: seq[float|int]): float =
+  if data.len mod 2 == 0:
+    return medianEven(data)
+  else:
     return medianOdd(data)
+
+assert median(@[10, 34, 56, 89, 99, 101, 102]) == 89
+assert median(@[10, 34, 54, 56, 89, 99, 101, 102]) == 72.5
 
 echo "Average: ", average(data)
 echo "Mean: ", mean(data)
