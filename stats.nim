2d2bfbec1ad1a5bcca81167ee5e8e13f2a110576
Alexis <alexis@cvofhope.com>
2019-08-17 09:34:42 +1000

    add proc median even

--- stats.nim
+++ stats.nim
@@ -9,7 +9,19 @@ const mean = average
 
 proc medianOdd(data: seq[int]): int =
   let nth = (data.len + 1) div 2
-  return data[nth]
+  return data[nth-1]
+
+assert medianOdd(@[0, 1, 2, 3, 4, 5, 6]) == 3
+assert medianOdd(@[10, 34, 56, 89, 99, 101, 102]) == 89
+
+proc medianEven(data: seq[float|int]): float =
+  let nth: float = data.len / 2
+  let lower:int = int(nth - 0.5)
+  let upper:int = int(nth + 0.5)
+  return (data[lower] + data[upper]).float / 2.float
+
+assert medianEven(@[0.0,1.0,2.0,3.0,4.0,5.0,6.0,7.0]) == 3.5
+assert medianEven(@[10, 34, 54, 56, 89, 99, 101, 102]) == 72.5
 
 proc median(data: seq[int]): int =
     return medianOdd(data)
