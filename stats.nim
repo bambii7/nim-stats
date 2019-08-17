122bfbfb844bc96c7608f9e17a051999a4283d76
Alexis <alexis@cvofhope.com>
2019-08-17 17:54:26 +1000

    add num type

--- stats.nim
+++ stats.nim
@@ -2,19 +2,22 @@ import math
 
 let data = @[0,1,2,3,4,5,6]
 
+type
+  num = int | float
+
 proc average(data: seq[int]): int =
   sum(data) div data.len
 
 const mean = average
 
-proc medianOdd(data: seq[float|int]): float =
+proc medianOdd(data: seq[num]): float =
   let nth:int = (data.len + 1) div 2
   return float(data[nth-1])
 
 assert medianOdd(@[0, 1, 2, 3, 4, 5, 6]) == 3
 assert medianOdd(@[10, 34, 56, 89, 99, 101, 102]) == 89
 
-proc medianEven(data: seq[float|int]): float =
+proc medianEven(data: seq[num]): float =
   let nth: float = data.len / 2
   let lower:int = int(nth - 0.5)
   let upper:int = int(nth + 0.5)
@@ -23,7 +26,7 @@ proc medianEven(data: seq[float|int]): float =
 assert medianEven(@[0.0,1.0,2.0,3.0,4.0,5.0,6.0,7.0]) == 3.5
 assert medianEven(@[10, 34, 54, 56, 89, 99, 101, 102]) == 72.5
 
-proc median(data: seq[float|int]): float =
+proc median(data: seq[num]): float =
   if data.len mod 2 == 0:
     return medianEven(data)
   else:
